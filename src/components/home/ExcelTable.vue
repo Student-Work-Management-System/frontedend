131ad28039c3310ac7e9acd4404f8c2a9db66f16
msqtt <mosquito@email.cn>
2024-05-16 21:08:47 +0800

    fix(exceltable): add new line when mount to fix table init width bug
    - format ExcelTable.vue

--- src/components/home/ExcelTable.vue
+++ src/components/home/ExcelTable.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { chLabel, type TableHeader } from '@/misc/table'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const model = defineModel()
 const props = defineProps<{ headers: TableHeader[]; nilData: any }>()
@@ -12,28 +12,18 @@ const addNewRow = () => {
 const deleteSelectRows = () => {
   tableRef.value.deleteSelectedRecords()
 }
+onMounted(() => {
+  tableRef.value.newRecord(JSON.parse(JSON.stringify(props.nilData)))
+})
 </script>
 <template>
   <v-btn variant="text" color="primary" @click="addNewRow">添加新行</v-btn>
   <v-btn variant="text" color="error" @click="deleteSelectRows">删除选行</v-btn>
-  <vue-excel-editor
-    v-model="model"
-    no-header-edit
-    ref="tableRef"
-    free-select
-    disable-panel-setting
-    :localized-label="chLabel"
-  >
-    <vue-excel-column
-      :key="idx"
-      v-for="(header, idx) in props.headers"
-      :field="header.field"
-      :label="`${header.require ? '<span style=\'color: red;\'>*</span>' : ''}` + header.label"
-      :type="header.type"
-      :options="header.options"
-      autoFillWidth
-      :validate="header.validate === null ? () => {} : header.validate"
-      key-field
-    ></vue-excel-column>
+  <vue-excel-editor v-model="model" no-header-edit ref="tableRef" free-select disable-panel-setting
+    :localized-label="chLabel">
+    <vue-excel-column :key="idx" v-for="(header, idx) in props.headers" :field="header.field"
+      :label="`${header.require ? '<span style=\'color: red;\'>*</span>' : ''}` + header.label" :type="header.type"
+      :options="header.options" autoFillWidth :validate="header.validate === null ? () => { } : header.validate"
+      key-field></vue-excel-column>
   </vue-excel-editor>
 </template>
